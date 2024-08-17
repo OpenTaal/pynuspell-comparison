513ac2f28e5b5632c236c9fc6df7340dce614805
Pander <pander@users.sourceforge.net>
2024-08-17 23:32:09 +0200

    added extra tests

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Comparing Python moduel for Nuspell with those for Hunspell
 
 ## Installation
 
-     sudo apt-get install libnuspell5 libhunspell-1.7
-     hunspell-en-us hunspell-es \
-     hunspell-nl wamerican wspanish wdutch
+     sudo apt-get install libnuspell5 libhunspell-1.7 \
+     hunspell-en-us hunspell-es hunspell-de-de hunspell-nl \
+     wamerican wspanish wngerman wdutch
      pip install -U pandas matplotlib pynuspell cyhunspell-py310
 
 ## Running
@@ -21,21 +21,27 @@ and the results are in PNG files. Note the break to limit total test time for su
 
 Spell
 
-[![](en_US-american-english-spell.png)](en_US-american-english-spell.png)
-[![](es_ES-spanish-spell.png)](es_ES-spanish-spell.png)
-[![](nl-dutch-spell.png)](nl-dutch-spell.png)
+[![](spell-en_US-american-english.png)](spell-en_US-american-english.png)
+[![](spell-es_ES-spanish.png)](spell-es_ES-spanish.png)
+[![](spell-de_DE-ngerman.png)](spell-de_DE-ngerman.png)
+[![](spell-nl-dutch.png)](spell-nl-dutch.png)
+[![](spell-nl-corrections.png)](spell-nl-corrections.png)
 
 Suggest
 
-[![](en_US-american-english-suggest.png)](en_US-american-english-suggest.png)
-[![](es_ES-spanish-suggest.png)](es_ES-spanish-suggest.png)
-[![](nl-dutch-suggest.png)](nl-dutch-suggest.png)
+[![](suggest-en_US-american-english.png)](suggest-en_US-american-english.png)
+[![](suggest-es_ES-spanish.png)](suggest-es_ES-spanish.png)
+[![](suggest-de_DE-ngerman.png)](suggest-de_DE-ngerman.png)
+[![](suggest-nl-dutch.png)](suggest-nl-dutch.png)
+[![](suggest-nl-corrections.png)](suggest-nl-corrections.png)
 
 ## See also
 
 See also:
+- https://nuspell.github.io/
 - https://pypi.org/project/pynuspell/
 - https://pypi.org/project/cyhunspell-py310/
+- https://github.com/OpenTaal/opentaal-wordlist
 
 Old:
 - https://pypi.org/project/hunspell/
--- compare.py
+++ compare.py
@@ -7,9 +7,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from pynuspell import load_from_path
-# hunspell
-# from hunspell import HunSpell
-# cyhunspell-py310
 from hunspell import Hunspell
 
 BASE_W: str = '/usr/share/dict/'
@@ -17,6 +14,7 @@ BASE_H: str = '/usr/share/hunspell/'
 LISTS: dict = {
     'en_US': 'american-english',
     'es_ES': 'spanish',
+    'de_DE': 'ngerman',
     'nl': 'dutch',
 }
 
@@ -30,9 +28,6 @@ def get_nuspell(code: str):
 
 def get_hunspell(code: str):
     """Get Hunspell."""
-    # hunspell
-    # return HunSpell(f'{BASE_H}{code}.dic', f'{BASE_H}{code}.aff')
-    # cyhunspell-py310
     if code == 'en_US':
         return Hunspell(code)
     return Hunspell(code, hunspell_data_dir=BASE_H)  # TODO Create issue
@@ -45,7 +40,8 @@ def histo(code: str, file: str, name_n: str, name_h: str, function: str,
     plt.figure(figsize=(19.20, 10.80))
     df.plot.hist(alpha=0.5, bins=32, edgecolor='black',
                  color=['blue', 'orange'], figsize=(19.20, 10.80))
-    plt.title(f'Histograms {function} with {code} on {file} for {data["len_h"]} words')
+    plt.title(f'Histograms {function} with {code} on {file} for'
+              f' {data["len_h"]} words')
     if function == 'suggest':
         plt.xlabel('Process Time [µs]')
     else:
@@ -53,61 +49,88 @@ def histo(code: str, file: str, name_n: str, name_h: str, function: str,
     plt.ylabel('Frequency [log]')
     plt.yscale('log')
     plt.grid(True)
-    leg_h = f'{name_h} min={int(data["min_h"]):,d} max={int(data["max_h"]):,d} total={int(data["tot_h"]):,d}'
-    leg_n = f'{name_n} min={int(data["min_n"]):,d} max={int(data["max_n"]):,d} total={int(data["tot_n"]):,d}'
+    leg_h = f'{name_h} total={int(data["tot_h"]):,d}' \
+            f' min={int(data["min_h"]):,d}' \
+            f' max={int(data["max_h"]):,d} for {data["slo_h"]}'
+    leg_n = f'{name_n} total={int(data["tot_n"]):,d}' \
+            f' min={int(data["min_n"]):,d}' \
+            f' max={int(data["max_n"]):,d} for {data["slo_n"]}'
     plt.legend([leg_h, leg_n])
-    plt.savefig(f'{code}-{file}-{function}.png')
+    plt.savefig(f'{function}-{code}-{file.replace(".tsv", "")}.png')
 
 
 def check(path, checker):
     """Check all words with spelling checker and get suggestions for fails."""
     res = {}
     tim_spe = []
-    if path.endswith('.tsv'):
-        exit(1)
     with open(path) as file:
-        failed = set()
+        incorrect = set()
+        max_spe = 0
+        slo_spe = ''
         cnt = 0
         for line in file:
-            line = line.strip()
+            line = line.strip().split('\t')[0]
             if line != '':
                 start = time.process_time_ns()
                 tmp = checker.spell(line)
-                tim_spe.append(time.process_time_ns() - start)
+                tim = time.process_time_ns() - start
+                if tim > max_spe:
+                    max_spe = tim
+                    slo_spe = line
+                tim_spe.append(tim)
                 if not tmp:
-                    failed.add(line)
+                    incorrect.add(line)
             cnt += 1
             if cnt % 50000 == 0:
                 print(f'    spe {cnt:07d}')
+        if cnt % 50000:
+            print(f'    spe {cnt:07d}')
     res['len_spe'] = len(tim_spe)
     res['min_spe'] = min(tim_spe)
-    res['max_spe'] = max(tim_spe)
+    res['max_spe'] = max_spe
     res['tot_spe'] = sum(tim_spe)
     res['tim_spe'] = tim_spe
+    res['slo_spe'] = slo_spe
 
     tim_sug = []
+    max_sug = 0.0
+    slo_sug = ''
     cnt = 0
-    for line in failed:
+    for line in incorrect:
         start = time.process_time_ns()
         checker.suggest(line)
-        tim_sug.append((time.process_time_ns() - start) / 1000)  # microseconds
+        tim = float(time.process_time_ns() - start) / 1000  # microseconds
+        if tim > max_sug:
+            max_sug = tim
+            slo_sug = line
+        tim_sug.append(tim)
         cnt += 1
         if cnt % 50 == 0:
             print(f'    sug {cnt:07d}')
-        if cnt == 500:
-            break
+        if path.endswith('dutch') or path.endswith('.tsv'):
+            if cnt == 150:
+                break
+        else:
+            if cnt == 500:
+                break
+    if cnt % 50:
+        print(f'    sug {cnt:07d}')
     res['len_sug'] = len(tim_sug)
     res['min_sug'] = min(tim_sug)
     res['max_sug'] = max(tim_sug)
     res['tot_sug'] = sum(tim_sug)
     res['tim_sug'] = tim_sug
+    res['slo_sug'] = slo_sug
 
     return res
 
 
 def compare(code: str, file: str) -> None:
     """Compare spelling checking and suggestions."""
-    path = f'{BASE_W}{file}'
+    if file.endswith('.tsv'):
+        path = f'../opentaal-wordlist/elements/{file}'
+    else:
+        path = f'{BASE_W}{file}'
     name_h = 'cyhunspell-py310'
     print(f'  {name_h}')
     res_h = check(path, get_hunspell(code))
@@ -121,11 +144,13 @@ def compare(code: str, file: str) -> None:
     spe['max_h'] = res_h['max_spe']
     spe['tot_h'] = res_h['tot_spe']
     spe['tim_h'] = res_h['tim_spe']
+    spe['slo_h'] = res_h['slo_spe']
     spe['len_n'] = res_n['len_spe']
     spe['min_n'] = res_n['min_spe']
     spe['max_n'] = res_n['max_spe']
     spe['tot_n'] = res_n['tot_spe']
     spe['tim_n'] = res_n['tim_spe']
+    spe['slo_n'] = res_n['slo_spe']
     histo(code, file, name_n, name_h, 'spell', spe)
 
     sug = {}
@@ -134,11 +159,13 @@ def compare(code: str, file: str) -> None:
     sug['max_h'] = res_h['max_sug']
     sug['tot_h'] = res_h['tot_sug']
     sug['tim_h'] = res_h['tim_sug']
+    sug['slo_h'] = res_h['slo_sug']
     sug['len_n'] = res_n['len_sug']
     sug['min_n'] = res_n['min_sug']
     sug['max_n'] = res_n['max_sug']
     sug['tot_n'] = res_n['tot_sug']
     sug['tim_n'] = res_n['tim_sug']
+    sug['slo_n'] = res_n['slo_sug']
     histo(code, file, name_n, name_h, 'suggest', sug)
 
 
@@ -146,4 +173,7 @@ if __name__ == '__main__':
     for code, file in sorted(LISTS.items()):
         print(f'{code} {file}')
         compare(code, file)
-    # compare(code, '../opentaal-wordlist/elements/corrections.tsv')
+    code = 'nl'
+    file = 'corrections.tsv'
+    print(f'{code} {file}')
+    compare(code, file)
